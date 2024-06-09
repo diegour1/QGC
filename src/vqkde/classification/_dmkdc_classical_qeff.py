af0c12f40c227b94cb30329616b4c2af23e4e598
Diego Hernando Useche Reyes <diego.h.useche@gmail.com>
2024-06-09 12:15:10 -0500

    changed dmkdc:_qeff

--- src/vqkde/classification/_dmkdc_classical_qeff.py
+++ src/vqkde/classification/_dmkdc_classical_qeff.py
@@ -16,17 +16,24 @@ def classical_dmkdc_qeff(X_train_param, Y_train_oh_param, X_test_param, n_qrffs_
   Y_train_oh_param = np.array(Y_train_oh_param)
   num_classes_temp = Y_train_oh_param.shape[1]
   Y_pred_temp = np.zeros((len(X_test_param), num_classes_temp))
+  sigma_temp = 1./(np.sqrt(2.*gamma_param))
 
   ## Training
   dmkdc_temp = []
   fm_x = QFeatureMapQuantumEnhancedFF(dim_x_temp, dim=n_qrffs_param, gamma=gamma_param, random_state= rs_param)
   for j in range(num_classes_temp):
-    dmkdc_temp.append(qmc_models.ComplexQMDensity(fm_x, n_qrffs_param))
+    try:
+      dmkdc_temp.append(qmc_models.ComplexQMDensity(fm_x, n_qrffs_param))
+    except:
+      pass
 
   ## Prediction
   for j in range(num_classes_temp):
-    dmkdc_temp[j].compile()
-    dmkdc_temp[j].fit(X_train_param[Y_train_oh_param[:, j].astype(bool)], epochs=1, batch_size = 1) ### must keep the batch size = 1
-    Y_pred_temp[:, j] = ((gamma_param/np.pi)**(dim_x_temp/2))*(dmkdc_temp[j].predict(X_test_param, batch_size = 1)) ### must keep the batch size = 1
-  
+    try:
+      dmkdc_temp[j].compile()
+      dmkdc_temp[j].fit(X_train_param[Y_train_oh_param[:, j].astype(bool)], epochs=1, batch_size = 1) ### must keep the batch size = 1
+      Y_pred_temp[:, j] = ((gamma_param/np.pi)**(dim_x_temp/2))*(dmkdc_temp[j].predict(X_test_param, batch_size = 1)) ### must keep the batch size = 1
+    except:
+      pass
+
   return ((Y_train_oh_param.sum(axis=0)/len(Y_train_oh_param)))*Y_pred_temp
