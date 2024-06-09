74c2039486707c6ca21e3fb5c320bb1fc40b38ce
Diego Hernando Useche Reyes <diego.h.useche@gmail.com>
2024-06-09 10:11:11 -0500

    changed names

--- src/vqkde/classification/__init__.py
+++ src/vqkde/classification/__init__.py
@@ -1,6 +1,6 @@
 from . import HEA
-from ._dmkde_classical_qeff import classical_dmkdc_qeff
-from ._dmkde_classical_qrff import classical_dmkdc_qrff
+from ._dmkdc_classical_qeff import classical_dmkdc_qeff
+from ._dmkdc_classical_qrff import classical_dmkdc_qrff
 from ._vqkdc_mixed_qeff import VQKDC_MIXED_QEFF
 from ._vqkdc_mixed_qrff import VQKDC_MIXED_QRFF
 
