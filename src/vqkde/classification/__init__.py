8cbec12d947b69ad9c51f437894abe835a74b75c
Diego Hernando Useche Reyes <diego.h.useche@gmail.com>
2024-06-09 11:10:32 -0500

    added kdc in classification __init__

--- src/vqkde/classification/__init__.py
+++ src/vqkde/classification/__init__.py
@@ -3,11 +3,13 @@ from ._dmkdc_classical_qeff import classical_dmkdc_qeff
 from ._dmkdc_classical_qrff import classical_dmkdc_qrff
 from ._vqkdc_mixed_qeff import VQKDC_MIXED_QEFF
 from ._vqkdc_mixed_qrff import VQKDC_MIXED_QRFF
+from ._kdc_classical import kernel_density_classification
 
 __all__ = [
     'HEA',
-    'classical_dmkdc_qeff',
+    'kernel_density_classification',
     'classical_dmkdc_qrff',
+    'classical_dmkdc_qeff',
     'VQKDC_MIXED_QEFF',
     'VQKDC_MIXED_QRFF'
 ]
